d8949eee06f305f54ee77cdfc929b22e25baf01f
Austen-Stuart <austen.stuart16@gmail.com>
2026-06-29 02:54:27 -1100

    Hellow i am adding

--- Git-test/sketch.js
+++ Git-test/sketch.js
@@ -1,7 +1,24 @@
 function setup() {
-    createCanvas(800, 600);
+    createCanvas(600, 600);
+
+    let answer = 42; //Declared a variable
+    let fav = 16; //My favorite number
+
+    let fruit1 = "Duran";
+    let fruit2 = "Kiwi";
+
+    console.log(answer + fav);
+    console.log(answer - fav);
+    console.log(answer * fav);
+    console.log(answer / fav);
+
+    console.log(fruit1 + " " + fruit2);
+    console.log(`I like ${fruit1} and ${fruit2}`);
+
+    const name = "Muse";
+    //name = "Austen"; //YOu cAnNoT Fix ME
 }
 
 function draw() {
-    background(220);
+    background(200);
 }
