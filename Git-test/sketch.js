8fb1ccc2c04e08fad1b4e4a2614c9600a27de907
Austen-Stuart <austen.stuart16@gmail.com>
2026-06-29 13:46:31 -1100

    Created First Project

--- Git-test/sketch.js
+++ Git-test/sketch.js
@@ -4,7 +4,7 @@ function setup() {
     let answer = 42; //Declared a variable
     let fav = 16; //My favorite number
 
-    let fruit1 = "Duran";
+    let fruit1 = "Check";
     let fruit2 = "Kiwi";
 
     console.log(answer + fav);
@@ -21,4 +21,24 @@ function setup() {
 
 function draw() {
     background(200);
+
+    fill ('black');
+    stroke('purple');
+    strokeWeight(10);
+    circle(300, 300, 300);
+    fill ('yellow');
+    stroke('yellow');
+    strokeWeight(0);
+    circle(300, 300, 250);
+    //eye 1
+    fill('black');
+    arc(235, 300, 110, 110, PI+(PI/6),2*PI+(PI/6));
+    fill ('cyan');
+    arc(255, 290, 10, 20, 0,2*PI);
+    //eye 2
+    fill('black');
+    arc(365, 300, 110, 110, (PI/6),PI+(PI/6));
+    fill ('cyan');
+    arc(365, 320, 10, 20, 0,2*PI);
+    noLoop();
 }
